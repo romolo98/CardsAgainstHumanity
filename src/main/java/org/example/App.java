f3786bd9a2dc09f6bb3edc0c3d30642acba6fe77
AlexJuju <account4s@live.it>
2020-12-28 18:20:17 +0100

    Lolicon No Push

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "NIENTE ROGER NE ODEN!!" );
+        System.out.println( "NIENTE CTRL+SPAZIO T_T");
     }
 }
