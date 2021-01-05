1cf55eb615e10ba36a410bfc99b714e1f562af55
Matteo <donnini.mat@gmail.com>
2021-01-05 18:13:52 +0100

    Versione funzionante

---
+++ src/main/sample/Controller.java
@@ -0,0 +1,14 @@
+package main.sample;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void ActionPlaybutton(ActionEvent actionEvent) {
+    }
+
+    public void ActionCreateButton(ActionEvent actionEvent) {
+    }
+
+    public void ActionOptionButton(ActionEvent actionEvent) {
+    }
+}
