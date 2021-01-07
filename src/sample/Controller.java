6b7117f82a5bb320f297208036d03d2441a8daf0
Jroms <romolo.aiello1998@gmail.com>
2021-01-07 18:02:13 +0100

    Sono state inserite le varie pagine del game. ovviamente ancora sono incomplete, e andrebbero riviste alcune cose, ad esempio il tasto torna indietro. Credo anche che vada migliorata la gestione delle scene in modo più efficiente. Per ora sono riuscito a fare questo :).

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,14 +1,58 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Controller {
-    public void ActionPlayButton(ActionEvent actionEvent) {
+
+    @FXML
+    Button playButton, createButton, optionsButton, turnBack, turnBack2, turnBack3;
+
+    public void ActionPlayButton(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("PlayScreen.fxml"));
+        Stage window = (Stage)playButton.getScene().getWindow();
+        window.setScene(new Scene(root));
     }
 
-    public void ActionCreatebutton(ActionEvent actionEvent) {
+    public void ActionCreatebutton(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("CreateRoomScreen.fxml"));
+        Stage window = (Stage)createButton.getScene().getWindow();
+        window.setScene(new Scene(root));
     }
 
-    public void ActionOptionsButton(ActionEvent actionEvent) {
+    public void ActionOptionsButton(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("OptionScreen.fxml"));
+        Stage window = (Stage)optionsButton.getScene().getWindow();
+        window.setScene(new Scene(root));
     }
+
+    public void ActionTurnBack(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("StartScreen.fxml"));
+        Stage window = (Stage)turnBack.getScene().getWindow();
+        window.setScene(new Scene(root));
+    }
+
+    public void ActionTurnBack2(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("StartScreen.fxml"));
+        Stage window = (Stage)turnBack2.getScene().getWindow();
+        window.setScene(new Scene(root));
+    }
+
+    public void ActionTurnBack3(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("StartScreen.fxml"));
+        Stage window = (Stage)turnBack3.getScene().getWindow();
+        window.setScene(new Scene(root));
+    }
+
+    public void ActionCreateRoomButton(ActionEvent actionEvent) {
+    }
+
+
 }
