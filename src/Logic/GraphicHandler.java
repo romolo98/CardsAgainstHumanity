6afbc97549207f8fa7135e7bd1062c0776358023
AlexJuju <account4s@live.it>
2021-01-19 16:31:47 +0100

    Edit GraphicHandler

--- src/Logic/GraphicHandler.java
+++ src/Logic/GraphicHandler.java
@@ -1,4 +1,62 @@
 package Logic;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import java.io.IOException;
+
 public class GraphicHandler {
+
+    private Parent root = null;
+    private Scene scene = null;
+
+    public static final int MAIN_SCREEN = 0;
+    public static final int PLAY_SCREEN = 1;
+    public static final int DECK_LIST_SCREEN = 2;
+    public static final int DECK_EDIT_SCREEN = 3;
+
+    private void init () {
+        if ( scene == null ) {
+            try {
+                root = FXMLLoader.load( getClass().getResource("StartScreen.fxml") );
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            scene.setRoot( root );
+        }
+    }
+
+    public Scene getScene () {
+        init();
+        return scene;
+    }
+
+    public void displayScreen ( int display ) {
+        String file_name;
+        switch ( display ) {
+            case MAIN_SCREEN:
+                file_name = "StartScreen.fxml";
+                break;
+            case PLAY_SCREEN:
+                file_name = "PlayScreen.fxml";
+                break;
+            case DECK_LIST_SCREEN:
+                file_name = "DeckManager.fxml";
+                break;
+            case DECK_EDIT_SCREEN:
+                file_name = "DeckScreen.fxml";
+                break;
+            default:
+                System.out.println("Cannot find selected display.");
+                return;
+        }
+
+        try {
+            root = FXMLLoader.load( getClass().getResource(file_name) );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        scene.setRoot( root );
+    }
 }
