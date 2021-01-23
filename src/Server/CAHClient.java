4aa104950832941bb310bbea84c8f97edd2a4c30
Nocturnal23 <matarazzogennaro1997@gmail.com>
2021-01-23 11:37:11 +0100

    Impostato CHAClient come Main

--- src/Server/CAHClient.java
+++ src/Server/CAHClient.java
@@ -1,5 +1,8 @@
 package Server;
 
+import Server.CAHNetwork;
+import Server.Messaggio;
+import Server.Mossa;
 import com.esotericsoftware.kryonet.Client;
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
@@ -11,6 +14,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import sample.Controller;
+import sample.DBConnector;
 
 
 import static Server.CAHNetwork.porta;
@@ -22,6 +26,7 @@ public class CAHClient extends Application {
     String host;
     static public Client client;
     private Controller controller;
+    private FXMLLoader loader = new FXMLLoader();
 
     public CAHClient () {
         client = new Client();
@@ -59,17 +64,17 @@ public class CAHClient extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader();
-        Parent root = loader.load(getClass().getResource("/sample/StartScreen.fxml").openStream());
-        primaryStage.setTitle("Client1");
+        Parent root = loader.load(getClass().getResource("../sample/StartScreen.fxml").openStream());
+        primaryStage.setTitle("Cards Against Humanity");
         primaryStage.setScene(new Scene(root));
         primaryStage.setMaximized(true);
         primaryStage.show();
+        DBConnector.getInstance().connect();
 
         controller = loader.getController();
 
         //BISOGNA SETTARE IL CLIENT
-        client.connect(5000, "localhost", porta);
+        //client.connect(5000, "localhost", porta);
 
         primaryStage.setOnCloseRequest(event -> {
             client.stop();
